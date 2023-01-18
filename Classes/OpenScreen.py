ba22da7e49bf624f3b5482c2f98896aba2938a47
Arina2412 <arinaglushkov@gmail.com>
2023-01-18 18:48:53 +0200

    Adding things

--- Classes/OpenScreen.py
+++ Classes/OpenScreen.py
@@ -4,33 +4,38 @@ from PIL import ImageTk, Image
 import threading
 import socket
 from Db_classes import *
+from SignupScreen import SignupScreen
 from tkinter import ttk, messagebox
 
 
 class StartScreen(tkinter.Tk):
-    def __init__(self,ip,port):
-        self.ip = ip
-        self.port = port
-
+    def __init__(self):
+        self.UserDb=UsersDb()
         super().__init__()
-        img = Image.open(r'C:\Users\ארינה\PycharmProjects\project_recipes\Classes\background.png')
-        bg = ImageTk.PhotoImage(img)
-        self.geometry('600x700')
+
+        self.geometry("600x700")
         self.title('Start Screen')
+
         #self.configure(bg="#BCEAD5")
         self.resizable(False,False)
 
-        Label(self, image=bg).place(x=0, y=0)
-
+        self.handle_thread_socket()
         self.create_gui()
     def create_gui(self):
-        self.L1=Label(self,text="Tasty Pages",background="#BCEAD5",foreground="Green",font=("Calibri",30))
+        self.img = Image.open('background.png')
+        self.resized = self.img.resize((600, 700), Image.LANCZOS)
+        self.image = ImageTk.PhotoImage(self.resized)
+        self.label_image = Label(self, image=self.image)
+        self.label_image.place(x=0, y=0)
+        # ________________________________________________________________________________________________________
+        self.L1=Label(self,text="Tasty Pages",background="#658864",foreground="white",font=("Calibri",30))
         self.L1.place(x=200,y=180)
-
-        self.buttonLogin=Button(self,text='LOG IN',background="green",foreground="white",font=("Calibri",15),command=self.open_login_screen)
+        # ________________________________________________________________________________________________________
+        self.buttonLogin=Button(self,text='LOG IN',background="#C27664",foreground="white",font=("Calibri",15),command=self.open_login_screen)
         self.buttonLogin.place(x=230,y=300,width=140,height=50)
-        self.buttonSignup=Button(self, text='SIGN UP', background="green",foreground="white",font=("Calibri",15),command=self.open_signup_screen)
+        self.buttonSignup=Button(self, text='SIGN UP', background="#C27664",foreground="white",font=("Calibri",15),command=self.open_signup_screen)
         self.buttonSignup.place(x=230,y=370,width=140,height=50)
+        # ________________________________________________________________________________________________________
 
     def open_login_screen(self):
         window=LoginScreen(self)
@@ -49,7 +54,7 @@ class StartScreen(tkinter.Tk):
 
     def create_socket(self):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect((self.ip, self.port))
+        self.client_socket.connect(('127.0.0.1',1803))
         data = self.client_socket.recv(1024).decode()
         print("data"+data)
         print("hi", self.client_socket)
@@ -61,32 +66,41 @@ class LoginScreen(tkinter.Toplevel):
         self.parent=parent
         self.geometry('600x700')
         self.title('LogIn Screen')
-        self.configure(bg="#BCEAD5")
+        self.resizable(False, False)
+        #self.configure(bg="#BCEAD5")
         self.UserDb = UsersDb()
 
         self.create_gui()
 
     def create_gui(self):
-        self.lblLogin = Label(self, text="Log In", foreground="Green", font=("Calibri", 30),background="#BCEAD5")
+        self.img = Image.open('background.png')
+        self.resized = self.img.resize((600, 700), Image.LANCZOS)
+        self.image = ImageTk.PhotoImage(self.resized)
+        self.label_image = Label(self, image=self.image)
+        self.label_image.place(x=0, y=0)
+        # ________________________________________________________________________________________________________
+        self.lblLogin = Label(self, text="Log In", foreground="white",background="#658864", font=("Calibri", 30))
         self.lblLogin.place(x=250, y=150)
-
-        self.lblUsernameLogin = Label(self, text="Username", foreground="Black", font=("Calibri", 15),background="#BCEAD5")
-        self.lblUsernameLogin.place(x=100, y=250)
+        # ________________________________________________________________________________________________________
+        self.lblUsernameLogin = Label(self, text="Username", foreground="Black",background="#C27664",font=("Calibri", 14))
+        self.lblUsernameLogin.place(x=100, y=245)
         self.entryUsernameLogin = Entry(self, width=70)
         self.entryUsernameLogin.place(x=95, y=280)
-
-        self.lblPasswordLogin = Label(self, text="Password", foreground="Black", font=("Calibri", 15),background="#BCEAD5")
-        self.lblPasswordLogin.place(x=100, y=350)
-        self.entryPasswordLogin = Entry(self, width=70)
+        self.entryUsernameLogin.insert(0, "Enter your username...")
+        self.entryUsernameLogin.config(fg="grey")
+        # ________________________________________________________________________________________________________
+        self.lblPasswordLogin = Label(self, text="Password", foreground="Black",background="#C27664",font=("Calibri", 14))
+        self.lblPasswordLogin.place(x=100, y=345)
+        self.entryPasswordLogin = Entry(self, width=70,show="*")
         self.entryPasswordLogin.place(x=95, y=380)
-
-        self.buttonEnterUserLogin = Button(self, text="Log In", background="green", foreground="white", font=("Calibri", 17),command=self.login_user)
+        # ________________________________________________________________________________________________________
+        self.buttonEnterUserLogin = Button(self, text="Log In", background="#C27664", foreground="white", font=("Calibri", 17),command=self.login_user)
         self.buttonEnterUserLogin .place(x=230, y=450, width=140, height=50)
-
-        self.buttonReturnToStartScreen = Button(self, text='Return Back', background="green", foreground="white", font=("Calibri", 15),
+        # ________________________________________________________________________________________________________
+        self.buttonReturnToStartScreen = Button(self, text='Return Back', background="#C27664", foreground="white", font=("Calibri", 14),
                           command=self.return_back)
         self.buttonReturnToStartScreen.place(x=245, y=550)
-
+        # ________________________________________________________________________________________________________
         self.str = StringVar()
         self.str.set("")
         Label(self, textvariable=self.str,foreground="red",font=("Calibri", 15)).place(x=250, y=400)
@@ -109,65 +123,6 @@ class LoginScreen(tkinter.Toplevel):
         self.parent.deiconify() #displays the window, after using the withdraw method
         self.destroy()
 
-class SignupScreen(tkinter.Toplevel):
-    def __init__(self,parent):
-        super().__init__(parent)
-        self.parent=parent
-        self.geometry('600x700')
-        self.title('SignUp Screen')
-        self.configure(bg="#BCEAD5")
-        self.UserDb=UsersDb()
-
-        self.create_gui()
-        self.buttonReturnToStartScreen2 = Button(self, text='Return Back', background="green", foreground="white",font=("Calibri", 15),command=self.return_back)
-        self.buttonReturnToStartScreen2.place(x=245, y=600)
-
-    def create_gui(self):
-        self.lblSignup = Label(self, text="Sign Up", foreground="Green", font=("Calibri", 30),background="#BCEAD5")
-        self.lblSignup.place(x=250, y=150)
-
-        self.lblEmailSignup = Label(self, text="Email", foreground="Black", font=("Calibri", 15),background="#BCEAD5")
-        self.lblEmailSignup.place(x=100, y=250)
-        self.entryEmailSignup = Entry(self, width=70)
-        self.entryEmailSignup.place(x=95, y=280)
-
-        self.lblUsernameSignup = Label(self, text="Username", foreground="Black", font=("Calibri", 15),background="#BCEAD5")
-        self.lblUsernameSignup.place(x=100, y=330)
-        self.entryUsernameSignup = Entry(self, width=70)
-        self.entryUsernameSignup .place(x=95, y=360)
-
-        self.lblPasswordSignup = Label(self, text="Password", foreground="Black", font=("Calibri", 15),background="#BCEAD5")
-        self.lblPasswordSignup.place(x=100, y=410)
-        self.entryPasswordSignup = Entry(self, width=70)
-        self.entryPasswordSignup.place(x=95, y=440)
-
-        self.buttonAddUserSignup = Button(self, text="Sign Up", background="green", foreground="white", font=("Calibri", 17),command=self.signup_user)
-        self.buttonAddUserSignup.place(x=230, y=500, width=140, height=50)
-
-
-    def handle_add_user(self):
-        self.client_handler = threading.Thread(target=self.signup_user, args=())
-        self.client_handler.daemon = True
-        self.client_handler.start()
-
-    def signup_user(self):
-        if len(self.entryEmailSignup.get())==0 or len(self.entryUsernameSignup.get())==0 or len(self.entryPasswordSignup.get())==0:
-            messagebox.showerror("Please write Email,Username and Password", "Error")
-            return
-        print("Sign Up")
-        arr = ["SignUp", self.entryEmailSignup.get(),self.entryUsernameSignup.get(),self.entryPasswordSignup.get()]
-        str_insert = ",".join(arr)
-        print(str_insert)
-        self.parent.client_socket.send(str_insert.encode())
-        data = self.parent.client_socket.recv(1024).decode()
-        print(data)
-
-    def return_back(self):
-        self.parent.deiconify()
-        self.destroy()
-
 if __name__ == "__main__":
-    ip = '127.0.0.1'
-    port = 1803
-    window = StartScreen(ip,port)
+    window = StartScreen()
     window.mainloop()
